6b2b4a7c2c07a3ba968afb2a717f4f84d8e8e4bb
jalvarad-1 <jalvarad-1@github.com>
2023-06-01 19:04:29 +0200

    probando cosas

--- srcs/requirements/wordpress/start.sh
+++ srcs/requirements/wordpress/start.sh
@@ -2,7 +2,7 @@
 
 
 # Configure WordPress
-wp config create --dbname=databasename --dbuser=root --dbpass=thisismyrootpassword --dbhost=127.0.0.1 --allow-root
+wp config create --dbname=databasename --dbuser=root --dbpass=thisismyrootpassword --dbhost=mariadb --allow-root
 
 # Install WordPress
 wp core install --url=jalvarad.42.fr --title=Inception --admin_user=admin --admin_password=1234 --admin_email=jalvarad@test.com --allow-root
