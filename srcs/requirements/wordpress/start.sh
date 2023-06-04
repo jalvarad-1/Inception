01902f2d9233e03eb49b6e822eeda7336dd4ab52
jalvarad-1 <jalvarad-1@github.com>
2023-06-04 16:28:16 +0200

    falta hacer VM y makefile

--- srcs/requirements/wordpress/start.sh
+++ srcs/requirements/wordpress/start.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 # Configure WordPress
+echo $DATABASENAME
 wp config create --dbname=$DATABASENAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$DB_HOST --allow-root
+echo pepe
 
 # Install WordPress
-wp core install --url=$DOMAIN_NAME --title=$PROJECT --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$LOGIN@test.com --allow-root
+wp core install --url=$DOMAIN_NAME --title=$PROJECT --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$LOGIN@inception.com --allow-root
 
 # Start php-fpm
 exec php-fpm8 -F
