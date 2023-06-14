df0563e3eed0b6987286a2b369a198706a50760c
jalvarad-1 <jalvarad-1@github.com>
2023-06-14 21:24:29 +0200

    debug wordpress comfig create

--- srcs/requirements/wordpress/start.sh
+++ srcs/requirements/wordpress/start.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 # If wp-config.php doesn't exist, configure and install WordPress
+
 if [ ! -f /var/www/html/wp-config.php ]; then
+    echo "entro al if"
     wp config create --dbname=$DATABASENAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$DB_HOST --allow-root
 fi
 wp core install --url=$DOMAIN_NAME --title=$PROJECT --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$LOGIN@inception.com --allow-root
 
 # Start php-fpm
 exec php-fpm8 -F
-
